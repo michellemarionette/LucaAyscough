69c69e0f0e29ce239f24e629f0899abf0aae3cca
mishell <lucaayscough@gmail.com>
2021-08-21 14:10:00 +0200

    Added close button.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.getElementById("contact_button").addEventListener("click", function(){
     document.querySelector(".contact").style.display = "inline"; 
 });
+
+document.getElementById("close_button").addEventListener("click", function(){
+    document.querySelector(".contact").style.display = "none"; 
+});
+
