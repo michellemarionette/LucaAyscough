31d9e522d88b34a2d6b5730a3cc1d3fc22292e44
Mishell <lucaayscough@gmail.com>
2021-08-25 16:58:12 +0200

    Changed layout.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-document.getElementById("contact_button").addEventListener("click", function(){
-    document.querySelector(".contact").style.display = "inline"; 
+function disablePages(){
+    document.querySelector("#about").style.display = "none";
+    document.querySelector("#discography").style.display = "none";
+    document.querySelector("#work").style.display = "none";
+    document.querySelector("#gravity").style.display = "none";
+
+    document.querySelector("#about_button").style.color = "#efefea";
+    document.querySelector("#discography_button").style.color = "#efefea";
+    document.querySelector("#work_button").style.color = "#efefea";
+    document.querySelector("#gravity_button").style.color = "#efefea";
+}
+
+function activatePage(name){
+    document.querySelector("#" + name).style.display = "inline"; 
+    document.querySelector("#" + name + "_button").style.color = "#acd9ff";
+}
+
+document.getElementById("about_button").addEventListener("click", function(){
+    disablePages();
+    activatePage("about"); 
 });
 
-document.getElementById("close_button").addEventListener("click", function(){
-    document.querySelector(".contact").style.display = "none"; 
+document.getElementById("work_button").addEventListener("click", function(){
+    disablePages();
+    activatePage("work"); 
 });
 
+document.getElementById("discography_button").addEventListener("click", function(){
+    disablePages();
+    activatePage("discography"); 
+});
+
+document.getElementById("gravity_button").addEventListener("click", function(){
+    disablePages();
+    activatePage("gravity"); 
+});
