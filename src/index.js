fab93021fe8cdf4a17b9e8af15c10e897c7111b6
Mishell <lucaayscough@gmail.com>
2021-08-26 10:58:36 +0200

    Added different menus.

--- src/index.js
+++ src/index.js
@@ -10,27 +10,47 @@ function disablePages(){
     document.querySelector("#gravity_button").style.color = "#efefea";
 }
 
-function activatePage(name){
+function enablePage(name){
     document.querySelector("#" + name).style.display = "inline"; 
     document.querySelector("#" + name + "_button").style.color = "#acd9ff";
 }
 
+function openMenu(){
+    document.querySelector(".header").style.display = "flex";
+    document.querySelector("#open_menu_button").style.display = "none";
+    document.querySelector("#close_menu_button").style.display = "inline";
+}
+
+function closeMenu(){
+    document.querySelector(".header").style.display = "none";
+    document.querySelector("#open_menu_button").style.display = "inline";
+    document.querySelector("#close_menu_button").style.display = "none";
+}
+
+document.getElementById("open_menu_button").addEventListener("click", function(){
+    openMenu();
+});
+
+document.getElementById("close_menu_button").addEventListener("click", function(){
+    closeMenu();
+});
+
 document.getElementById("about_button").addEventListener("click", function(){
     disablePages();
-    activatePage("about"); 
+    enablePage("about"); 
 });
 
 document.getElementById("work_button").addEventListener("click", function(){
     disablePages();
-    activatePage("work"); 
+    enablePage("work"); 
 });
 
 document.getElementById("discography_button").addEventListener("click", function(){
     disablePages();
-    activatePage("discography"); 
+    enablePage("discography"); 
 });
 
 document.getElementById("gravity_button").addEventListener("click", function(){
     disablePages();
-    activatePage("gravity"); 
+    enablePage("gravity"); 
 });
