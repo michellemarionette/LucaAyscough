f7297314bd706e060a50df73519edd307873e9e6
Mishell <lucaayscough@gmail.com>
2021-08-27 15:43:40 +0200

    Fixed bugs.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,117 @@
-function disablePages(){
-    document.querySelector("#about").style.display = "none";
-    document.querySelector("#discography").style.display = "none";
-    document.querySelector("#work").style.display = "none";
-    document.querySelector("#gravity").style.display = "none";
+// VARIABLES
 
-    document.querySelector("#about_button").style.color = "#efefea";
-    document.querySelector("#discography_button").style.color = "#efefea";
-    document.querySelector("#work_button").style.color = "#efefea";
-    document.querySelector("#gravity_button").style.color = "#efefea";
-}
+const about_page = document.querySelector(".about");
+const work_page = document.querySelector(".work");
+const discography_page = document.querySelector(".discography");
+const gravity_page = document.querySelector(".gravity");
 
-function enablePage(name){
-    document.querySelector("#" + name).style.display = "inline"; 
-    document.querySelector("#" + name + "_button").style.color = "#acd9ff";
+const about_button = document.querySelector("#about_button");
+const work_button = document.querySelector("#work_button");
+const discography_button = document.querySelector("#discography_button");
+const gravity_button = document.querySelector("#gravity_button");
+
+const header = document.querySelector(".header");
+const open_menu_button = document.querySelector(".open_menu_button");
+const close_menu_button = document.querySelector(".close_menu_button");
+
+const displayNone = "displayNone";
+const displayFlex = "displayFlex";
+const displayInline = "displayInline";
+const clearDisplay = "clearDisplay";
+const linkTextColorOn = "linkTextColorOn";
+
+
+// FUNCTIONS
+
+function switchDisplay(displayType, element){
+    for(let i = 0; i < element.classList.length; i++){
+        if(element.classList[i].startsWith("display")){
+            element.classList.remove(element.classList[i]);
+        }
+    }
+
+    if(displayType != clearDisplay){
+        element.classList.add(displayType);
+    }
 }
 
 function openMenu(){
-    document.querySelector(".header").style.display = "flex";
-    document.querySelector("#open_menu_button").style.display = "none";
-    document.querySelector("#close_menu_button").style.display = "inline";
+    switchDisplay(displayFlex, header);
+    switchDisplay(displayNone, open_menu_button);
+    switchDisplay(displayInline, close_menu_button);
 }
 
 function closeMenu(){
-    document.querySelector(".header").style.display = "none";
-    document.querySelector("#open_menu_button").style.display = "inline";
-    document.querySelector("#close_menu_button").style.display = "none";
+    switchDisplay(clearDisplay, header);
+    switchDisplay(clearDisplay, open_menu_button);
+    switchDisplay(clearDisplay, close_menu_button);
 }
 
-document.getElementById("open_menu_button").addEventListener("click", function(){
+open_menu_button.addEventListener("click", function(){
     openMenu();
 });
 
-document.getElementById("close_menu_button").addEventListener("click", function(){
+close_menu_button.addEventListener("click", function(){
     closeMenu();
 });
 
-document.getElementById("about_button").addEventListener("click", function(){
-    disablePages();
-    enablePage("about"); 
-});
+function disablePages(){
+    switchDisplay(displayNone, about_page);
+    switchDisplay(displayNone, work_page);
+    switchDisplay(displayNone, discography_page);
+    switchDisplay(displayNone, gravity_page);   
+    
+    about_button.classList.remove(linkTextColorOn);
+    work_button.classList.remove(linkTextColorOn);
+    discography_button.classList.remove(linkTextColorOn);
+    gravity_button.classList.remove(linkTextColorOn);
+}
 
-document.getElementById("work_button").addEventListener("click", function(){
-    disablePages();
-    enablePage("work"); 
-});
+function enablePage(page){
+    var name = page.classList[1];
+    switchDisplay(displayInline, page);
+    document.querySelector("#" + name + "_button").classList.add(linkTextColorOn);
+}
 
-document.getElementById("discography_button").addEventListener("click", function(){
-    disablePages();
-    enablePage("discography"); 
-});
 
-document.getElementById("gravity_button").addEventListener("click", function(){
-    disablePages();
-    enablePage("gravity"); 
-});
+// INIT FUNCTIONS
+
+function initListeners(){
+    open_menu_button.addEventListener("click", function(){
+        openMenu();
+    });
+    
+    close_menu_button.addEventListener("click", function(){
+        closeMenu();
+    });
+    
+    about_button.addEventListener("click", function(){
+        disablePages();
+        enablePage(about_page); 
+    });
+    
+    work_button.addEventListener("click", function(){
+        disablePages();
+        enablePage(work_page); 
+    });
+    
+    discography_button.addEventListener("click", function(){
+        disablePages();
+        enablePage(discography_page); 
+    });
+    
+    gravity_button.addEventListener("click", function(){
+        disablePages();
+        enablePage(gravity_page); 
+    });
+}
+
+function initPage(){
+    about_button.classList.add(linkTextColorOn);
+}
+
+
+// MAIN CODE
+
+initPage();
+initListeners();
