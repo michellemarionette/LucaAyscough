9457d20893c53968d3a88423cf5f45779f846531
Mishell <lucaayscough@gmail.com>
2021-09-24 15:58:33 +0100

    Added content.

--- src/pages/home.php
+++ src/pages/home.php
@@ -6,6 +6,9 @@
     </div>
 
     <div class="content_section">
+        <h3>FEATURED CONTENT</h3><br>
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1319737750%3Fsecret_token%3Ds-XE5jjv8JQJF&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        
         <div class="video_container">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/4SLPdiaT3Ns" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
@@ -14,8 +17,6 @@
         <p>
             I have been studying and playing music most of my life and I am fascinated by technology and its relationship to music and sound. I specialize in audio production and sound design and have worked in a large variety of creative and technical contexts allowing me to develop my skills as an engineer, sound designer and producer. I am also the creator of Gravity, an audio plugin which allows users to generate sounds in an unprecedented way.
         </p><br><br>
-
-        
         
         <div class="dropdown_button">
             <h4>View Curriculum Vitae</h4>
