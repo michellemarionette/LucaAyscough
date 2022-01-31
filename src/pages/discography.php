196dacbf16fab80f876c6fc15ea58941abc63e28
michellemarionette <lucaayscough@gmail.com>
2022-01-31 02:14:37 +0000

    Added content.

--- src/pages/discography.php
+++ src/pages/discography.php
@@ -18,5 +18,12 @@
             <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/933606349&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <iframe width="100%" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/608053029&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
         </div>
+
+        <center><h1>SHADOW</h1></center>
+        <div class="artist">
+            <iframe src="https://open.spotify.com/embed/track/0WOlRhQZPFq5hVLLCXfz0O?utm_source=generator" width="290" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <iframe src="https://open.spotify.com/embed/track/7rBwx1SZnOkTTPIH8FU0mt?utm_source=generator" width="290" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <iframe src="https://open.spotify.com/embed/track/5mg15TyfEE80vR7apia3zb?utm_source=generator" width="290" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
     </div>
 </div>
