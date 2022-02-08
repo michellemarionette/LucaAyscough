2feb48b3e5c6c90c78da830fe299624466239cb5
michellemarionette <lucaayscough@gmail.com>
2022-02-08 22:06:44 +0000

    Update home page

--- src/pages/home.php
+++ src/pages/home.php
@@ -7,7 +7,12 @@
 
     <div class="content_section">
         <h3>FEATURED CONTENT</h3><br>
+        
         <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1319737750%3Fsecret_token%3Ds-XE5jjv8JQJF&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        
+        <div class="video_container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/4ZzxE2pQYJ0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
         
         <div class="video_container">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/4SLPdiaT3Ns" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
