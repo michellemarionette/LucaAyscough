77aa08099a3894f67917f4d40ce4747962f4a31c
michellemarionette <lucaayscough@gmail.com>
2022-02-12 20:36:47 +0000

    Updated discography page

--- src/pages/discography.php
+++ src/pages/discography.php
@@ -6,20 +6,49 @@
     </div>
 
     <div class="content_section">
-        <center><h1>LUCA AYSCOUGH</h1></center>
+        <center><h2>LUCA AYSCOUGH</h2></center>
+
+        <div class="artist_links">
+            <a href="" target="_blank">SPOTIFY</a> | 
+            <a href="https://www.youtube.com/channel/UCLcEsYoqfcFq7CmSFUvSKOw" target="_blank">YOUTUBE</a> | 
+            <a href="" target="_blank">APPLE MUSIC</a> | 
+            <a href="https://lucaayscough.bandcamp.com/" target="_blank">BANDCAMP</a> | 
+            <a href="https://soundcloud.com/lucaayscough" target="_blank">SOUNDCLOUD</a> | 
+            <a href="https://www.instagram.com/lucaayscough/" target="_blank">INSTAGRAM</a>
+        </div>
+
         <div class="artist">
+            <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1213570996%3Fsecret_token%3Ds-pAuieh33uX2&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1295048353&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1205068081&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
         </div>
         
-        <center><h1>HAPPY FACE FACTORY</h1></center>
+        <center><h2>HAPPY FACE FACTORY</h2></center>
+
+        <div class="artist_links">
+            <a href="https://open.spotify.com/artist/5JTisxShIGc7gZZkdhlGZB?si=yJGBzcTLQ2OMYEOrpTMvGw" target="_blank">SPOTIFY</a> | 
+            <a href="https://www.youtube.com/channel/UCiiq6Los-KMsrvL2hRUnOLA" target="_blank">YOUTUBE</a> | 
+            <a href="https://music.apple.com/us/artist/happy-face-factory/1277967805" target="_blank">APPLE MUSIC</a> | 
+            <a href="https://happyfacefactory.bandcamp.com/" target="_blank">BANDCAMP</a> | 
+            <a href="https://soundcloud.com/happyfacefactory" target="_blank">SOUNDCLOUD</a> | 
+            <a href="https://www.instagram.com/happyfacefactory/" target="_blank">INSTAGRAM</a>
+        </div>
+
         <div class="artist">
             <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1309544041&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <iframe width="290" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/933606349&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <iframe width="100%" height="500" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/608053029&color=%232b3e46&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
         </div>
 
-        <center><h1>SHADOW</h1></center>
+        <center><h2>SHADOW</h2></center>
+
+        <div class="artist_links">
+            <a href="https://open.spotify.com/artist/3bSWYsU6y4xaKxiKjSPxbi?si=rjeW79FbS4G-YE2R2yXx3A" target="_blank">SPOTIFY</a> | 
+            <a href="https://www.youtube.com/channel/UC-M9RYP9OkdIKc9RbGTgBiA" target="_blank">YOUTUBE</a> | 
+            <a href="https://music.apple.com/fm/artist/shadow/1511984478" target="_blank">APPLE MUSIC</a> | 
+            <a href="https://www.instagram.com/tooshadovv/" target="_blank">INSTAGRAM</a>
+        </div>
+
         <div class="artist">
             <iframe src="https://open.spotify.com/embed/track/0WOlRhQZPFq5hVLLCXfz0O?utm_source=generator" width="290" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             <iframe src="https://open.spotify.com/embed/track/7rBwx1SZnOkTTPIH8FU0mt?utm_source=generator" width="290" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
